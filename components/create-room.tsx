abcf1e971237414175236c07ab881a4e0ab0695e
imdarkly <svit.darkly@gmail.com>
2024-10-19 12:26:24 +0300

    feat: redirecting to room

--- components/create-room.tsx
+++ components/create-room.tsx
@@ -4,21 +4,25 @@ import { Button } from "./ui/button";
 import { toast } from "sonner";
 import { useState } from "react";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function CreateRoom() {
     const supabase = createClient();
     const [isloading, setIsLoading] = useState(false);
+    const router = useRouter();
 
     const handleCreateRoom = async () => {
         setIsLoading(true);
 
+        let user;
+
         // Check if the user is already logged in
         const {
-            data: { user },
+            data: { user: existingUser },
         } = await supabase.auth.getUser();
 
         // If not logged in, attempt to log in anonymously
-        if (!user) {
+        if (!existingUser) {
             const { data: loginData, error: loginError } =
                 await supabase.auth.signInAnonymously();
             if (loginError) {
@@ -29,6 +33,9 @@ export default function CreateRoom() {
                 setIsLoading(false);
                 return;
             }
+            user = loginData.user;
+        } else {
+            user = existingUser;
         }
 
         // Attempt to generate code.
@@ -48,6 +55,7 @@ export default function CreateRoom() {
         const { data: roomData, error: roomError } = await supabase
             .from("rooms")
             .insert([{ room_code: roomCodeData, master_id: user?.id }]);
+        console.error("User id: ", user?.id);
         if (roomError) {
             toast.error("Unable to create room.", {
                 description:
@@ -56,8 +64,12 @@ export default function CreateRoom() {
             setIsLoading(false);
             return;
         }
+        toast.success("Room created successfully!", {
+            description: `Your room code is ${roomCodeData}`,
+        });
 
         setIsLoading(false);
+        router.push(`/room/${roomCodeData}/master`);
     };
 
     return (
